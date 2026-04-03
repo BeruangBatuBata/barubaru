508460dd9fb5f586616877be0dce9d9baddd2d70
BeruangBatuBata <92040313+BeruangBatuBata@users.noreply.github.com>
2026-04-03 21:11:57 +0700

    Update tournaments.py

--- utils/tournaments.py
+++ utils/tournaments.py
@@ -6,7 +6,7 @@ ALL_TOURNAMENTS = {
     'VMC 2026 Spring': {'path': 'Vietnam_MLBB_Championship/2026/Spring', 'region': 'Vietnam', 'year': 2026, 'live': True, 'league': 'VMC', 'split': '2026 Split 1'},
     'MPL MENA S8': {'path': 'MPL/MENA/Season_8', 'region': 'MENA', 'year': 2025, 'live': True, 'league': 'MPL MENA', 'split': '2025 Split 2'},
     'MCC S6': {'path': 'MLBB_Continental_Championships/Season_6', 'region': 'EECA', 'year': 2025, 'live': True, 'league': 'MCC (EECA)', 'split': '2025 Split 2'},
-    'China Masters 2025': {'path': 'MLBB_China_Masters/2025', 'region': 'China', 'year': 2025, 'live': True, 'league': 'China Masters', 'split': '2025 Split 2'},
+    'China Masters Season 2': {'path': 'MLBB_China_Masters/Season_2', 'region': 'China', 'year': 2026, 'live': True, 'league': 'China Masters', 'split': '2026 Split 1'},
     'MTC S6': {'path': 'MTC_Turkiye_Championship/Season_6', 'region': 'Turkey', 'year': 2025, 'live': True, 'league': 'MTC (Turkey)', 'split': '2025 Split 1'},
     'MPL KH Season 10': {'path': 'MPL/Cambodia/Season_10', 'region': 'Cambodia', 'year': 2026, 'live': True, 'league': 'MPL Cambodia', 'split': '2026 Split 1'},
     'MSL MM Season 3': {'path': 'MLBB_Super_League/Myanmar/Season_3', 'region': 'Myanmar', 'year': 2026, 'live': True, 'league': 'MSL Myanmar', 'split': '2026 Split 1'},
